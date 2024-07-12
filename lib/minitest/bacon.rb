564aa948e94049f5e9dc2605ad75997c928a7c24
Ryan Davis <ryand-ruby@zenspider.com>
2024-07-12 10:54:50 -0800

    prepped for release [git-p4: depot-paths = "//src/minitest-bacon/dev/": change = 14240]

--- lib/minitest/bacon.rb
+++ lib/minitest/bacon.rb
@@ -29,7 +29,7 @@ class Minitest::Assertion
 end
 
 class Minitest::ValueMonad
-  VERSION = "1.0.3"
+  VERSION = "1.0.4"
 
   instance_methods.each { |name| undef_method name  if name =~ /\?|^\W+$/ }
 
